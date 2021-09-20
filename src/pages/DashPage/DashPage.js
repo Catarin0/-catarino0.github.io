505b23d83376669a3bf5ef07feb1f9c9a9fcb922
Catarin0 <diogocatarino2014@gmail.com>
2021-09-20 19:13:36 +0100

    v1.2

--- src/pages/DashPage/DashPage.js
+++ src/pages/DashPage/DashPage.js
@@ -1,12 +1,17 @@
 import React, {FC} from 'react';
+import grossas from "../../images/grossas.gif"
+import './DashPage.css'
 
 const DashPage: FC = () => {
 
     return (
-        <div>
+        <div className="dash">
             <h1>GROSSAS</h1>
+            <div className="image">
+                <img id="img" src={grossas} alt="dsf"/>
+            </div>
         </div>
     );
 }
 
-export default DashPage;
+export default DashPage
